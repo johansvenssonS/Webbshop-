0f7277c5ce809837a299c8815dc5d9fe44c821e2
Johan Svensson <johan.svensson0608@gmail.com>
2025-12-05 13:31:31 +0100

    enskild-produkt sida

---
+++ johan/component/pSite.js
@@ -0,0 +1,28 @@
+class SingleProductView {
+  constructor(product) {
+    this.product = product;
+  }
+
+  renderView = (p) => {
+    //töm main
+    console.log("hej");
+    let main = document.querySelector("main");
+    main.innerHTML = "";
+
+    let c = document.createElement("div");
+
+    c.innerHTML = `
+    <div class="single-product">
+    <img src="${p.image}" class="single-product-image" />
+      <div class="single-product-text">
+        <a class="single-name">${p.name}</a>
+        <div class="singleP-price">Pris:${p.price}</div>
+        <p>${p.description}</p>
+      </div>
+    `;
+
+    main.appendChild(c);
+  };
+}
+
+export { SingleProductView };
