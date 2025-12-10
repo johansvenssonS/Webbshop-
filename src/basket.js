92a2e0f772e6efe1e987eba955e467ffdbb89775
Johan Svensson <johan.svensson0608@gmail.com>
2025-12-10 11:05:24 +0100

    lite basket klass

--- src/basket.js
+++ src/basket.js
@@ -3,12 +3,25 @@ class Basket {
     this.cart = [];
   }
 
-  addToBasket = () => {};
+  addToBasket = (item) => {
+    this.cart.push(item);
+  };
+
+  basketLength = () => {
+    return this.cart.length();
+  };
+
+  getBasket = () => {
+    return this.cart;
+  };
 
   getTotPrice = () => {
     let tot = 0;
     for (let p in this.basket) {
       tot += p.price;
     }
+    return tot;
   };
+
+  removeItem = (item) => {};
 }
